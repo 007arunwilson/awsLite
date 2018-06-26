92a35d1cfa453f77ff933b70ee7031cdd1c9a26e
007arunwilson <007arunwilson@gmail.com>
2018-06-26 15:25:42 +0530

    Updated the homepage

--- App.js
+++ App.js
@@ -4,19 +4,14 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, Image } from "react-native";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 type Props = {};
@@ -24,15 +19,18 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
+        <Image
+          style={{ width: 100, height: 100 }}
+          source={{
+            uri:
+              "https://a0.awsstatic.com/libra-css/images/logos/aws_logo_smile_1200x630.png"
+          }}
+        />
+        <Text style={styles.welcome}>Welcome to AWS Lite App!</Text>
         <Text style={styles.instructions}>
-          To get started, edit App.js
+          A Lite version of AWS Console which includes essentials.
         </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
   }
@@ -41,18 +39,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
